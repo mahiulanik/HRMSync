24ba33af331edf5d764ab18217cbe934ec351a77
mahiulanik <mahiulanik@gmail.com>
2026-08-13 22:45:16 +0600

    updated axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,14 +3,14 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: "/api",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
 });
 
 const refreshClient = axios.create({
-    baseURL: API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
